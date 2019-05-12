ec90065a6f058a51fc864873b9ecfc153f18aa36
Raphael Michel <mail@raphaelmichel.de>
2019-05-12 17:53:23 +0200

    Bump version

--- django_scopes/__init__.py
+++ django_scopes/__init__.py
@@ -2,7 +2,7 @@ from .exceptions import ScopeError
 from .manager import ScopedManager
 from .state import get_scope, scope, scopes_disabled
 
-version = '1.0.1'
+version = '1.1.0'
 
 __all__ = [
     'version',
