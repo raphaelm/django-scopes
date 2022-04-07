48e5584e471d6011e9565bea3cb8c560ddbe2a8f
Raphael Michel <michel@rami.io>
2022-04-07 11:18:17 +0200

    Bump to 1.2.0.post1 to trigger PyPI release

--- django_scopes/__init__.py
+++ django_scopes/__init__.py
@@ -2,7 +2,7 @@ from .exceptions import ScopeError
 from .manager import ScopedManager
 from .state import get_scope, scope, scopes_disabled
 
-version = '1.2.0'
+version = '1.2.0.post1'
 
 __all__ = [
     'version',
