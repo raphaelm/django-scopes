83e240b717a1a93efc61ebcb24712082878c32ff
Henryk Plötz <henryk@ploetzli.ch>
2023-03-10 14:29:17 +0100

    Replace threading.local with contextvars.ContextVar This allows usage of scopes with async code.

--- django_scopes/state.py
+++ django_scopes/state.py
@@ -1,7 +1,8 @@
 from contextlib import contextmanager
-from threading import local
+from contextvars import ContextVar
+from typing import Optional
 
-state = local()
+state: ContextVar[Optional[dict]] = ContextVar('state', default=None)
 
 
 @contextmanager
@@ -12,19 +13,20 @@ def scopes_disabled():
 
 @contextmanager
 def scope(**scope_kwargs):
-    if not hasattr(state, 'scope'):
-        state.scope = {}
+    previous_scope = state.get()
+    if previous_scope is None:
+        previous_scope = {}
+        state.set(previous_scope)
 
-    previous_scope = getattr(state, 'scope', {})
     new_scope = dict(previous_scope)
     new_scope['_enabled'] = True
     new_scope.update(scope_kwargs)
-    state.scope = new_scope
+    state.set(new_scope)
     try:
         yield
     finally:
-        state.scope = previous_scope
+        state.set(previous_scope)
 
 
 def get_scope():
-    return dict(getattr(state, 'scope', {}))
+    return state.get() or {}
