ad3e8ee9ce1031532ddec718c09c89a2affd40ca
Raphael Michel <mail@raphaelmichel.de>
2023-04-08 21:56:44 +0200

    Bump to 2.0.0

--- django_scopes/__init__.py
+++ django_scopes/__init__.py
@@ -2,7 +2,7 @@ from .exceptions import ScopeError
 from .manager import ScopedManager
 from .state import get_scope, scope, scopes_disabled
 
-version = '1.2.0.post1'
+version = '2.0.0'
 
 __all__ = [
     'version',
